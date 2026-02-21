b2d875a95f235ee879ef0ee874b5bab5b10c2c06
Zach Francis <zacharyjamesfrancis@gmail.com>
2026-02-20 18:32:59 -0700

    Complete foundations exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) return "ERROR";
+  ret = "";
+  for (let i = 0; i < num; i++) {
+    ret += str;
+  }
+  return ret;
 };
 
 // Do not edit below this line
