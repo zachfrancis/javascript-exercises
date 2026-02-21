e6e33a5bf29a764b915530869e87d12707b7b8b1
Zach Francis <zacharyjamesfrancis@gmail.com>
2026-02-20 19:01:02 -0700

    Complete foundations exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
 
+  // Ensure a is the lower value
+  if (a > b) [a, b] = [b, a];
+  let res = 0;
+  for (let i = a; i <= b; i++) {
+    res += i;
+  }
+  return res;
 };
 
 // Do not edit below this line
