950264f539f63771e155f157b389ff18d84fc0b0
Zach Francis <zacharyjamesfrancis@gmail.com>
2026-02-20 19:21:24 -0700

    Complete foundations exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp_f) {
+  temp_c = (temp_f - 32) * (5 / 9);
+  return Math.round(temp_c * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp_c) {
+  temp_f = temp_c * (9 / 5) + 32;
+  return Math.round(temp_f * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
