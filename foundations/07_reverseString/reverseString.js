a8396590ac11348828e2b2d71639e1a00df04452
Zach Francis <zacharyjamesfrancis@gmail.com>
2026-02-20 18:39:54 -0700

    Complete foundations exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  reverse = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverse += string[i];
+  }
+  return reverse;
 };
 
 // Do not edit below this line
