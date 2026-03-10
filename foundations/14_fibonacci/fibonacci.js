980b220d6d162f5b07accb7ec6409ba5ea4eb694
Zach Francis <zacharyjamesfrancis@gmail.com>
2026-03-09 23:46:38 -0600

    Complete lesson 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let n = Number(num);
 
+  if (n < 0) return "OOPS";
+  if (n == 0) return 0;
+
+  let prev = 0;
+  let curr = 1;
+
+  for (let i = 2; i <= n; i++) {
+    [prev, curr] = [curr, curr + prev];
+  }
+  return curr;
 };
 
 // Do not edit below this line
