02d1fd7ef3b2c8dba9d1d219bf73791bd2fa93ad
Zach Francis <zacharyjamesfrancis@gmail.com>
2026-03-09 23:28:06 -0600

    Complete lesson 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const trimString = str.replace(/[!,. ]/g, "").toLowerCase();
 
+  const start = trimString.slice(0, trimString.length / 2);
+  const end = trimString.slice(trimString.length / 2);
+  let endReversed = end.split("").reverse().join("");
+  if (endReversed.length > start.length) {
+    endReversed = endReversed.slice(0, -1);
+  }
+  return start === endReversed;
 };
 
 // Do not edit below this line
