b2f2d917f2898b0d89066a217b003a3825c7e3dc
Zach Francis <zacharyjamesfrancis@gmail.com>
2026-03-09 23:52:57 -0600

    Complete lesson 15

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  return books.reduce((arr, book) => {
+    arr.push(book.title);
+    return arr;
+  }, []);
 };
 
 // Do not edit below this line
