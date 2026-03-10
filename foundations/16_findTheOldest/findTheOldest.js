5f19091e213f57901b569adb5eff72f88c21f037
Zach Francis <zacharyjamesfrancis@gmail.com>
2026-03-10 00:13:48 -0600

    Complete lesson 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce(
+    (oldest, person) => {
+      if (!person.yearOfDeath) {
+        person.yearOfDeath = new Date().getFullYear();
+      }
+      const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+      const currentAge = person.yearOfDeath - person.yearOfBirth;
+      return currentAge > oldestAge ? person : oldest;
+    },
+    {
+      name: "NOBODY",
+      yearOfBirth: 0,
+      yearOfDeath: 0,
+    },
+  );
 };
 
 // Do not edit below this line
